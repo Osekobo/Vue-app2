a343f4cff452546d3023790d26448643d18bcf8c
Oseko <collinsoseko2003@gmail.com>
2026-09-07 13:53:58 +0300

    API_BASE_URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = 'http://localhost:8000'; 
-// export const API_BASE_URL = 'http://164.90.221.37:8000'; 
-export const API_BASE_URL = 'https://api.oberlo.co.ke'; 
+export const API_BASE_URL = 'http://164.90.221.37:8000'; 
+// export const API_BASE_URL = 'https://api.oberlo.co.ke'; 
